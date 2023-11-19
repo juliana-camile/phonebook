89fff66276727fc12fad77df5ba635334668cdeb
Elisabeth <elisabethtopitschh@gmail.com>
2023-11-18 21:26:17 -0300

    index

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,12 @@
 <?php
     include_once("config/url.php");
-    include_once("config/connection.php");
+    include_once("config/process.php");
+
+    // limpa a mensagem
+    if(isset($SESSION['msg'])) {
+        $printMsg = $SESSION['msg'];
+        $_SESSION['msg'] = '';
+    }
 ?>
 
 <!DOCTYPE html>
